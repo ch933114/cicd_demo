2daffb819d4d1f6adc2db369be09a395291ae25f
dean <dean.liu@omniguider.com>
2026-01-13 09:08:45 +0800

    modify: 調整base

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/cicd-demo/',
+  base: '/cicd_demo/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
